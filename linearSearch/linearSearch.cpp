eac038d9650e286c7257520453c91e68c4df1eb0
Nandhu <nandhu90040@gmail.com>
2023-09-20 15:04:51 +0530

    Linear Search

---
+++ linearSearch/linearSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using std::cout, std::cin, std::endl;
+
+int linearSearch(int array[], int n, int v)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (array[i] == v)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	int n, v;
+	cout << "Enter the size of the array: ";
+	cin >> n;
+	int array[n];
+	cout << "Enter the elements of the array: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> array[i];
+	}
+	cout << "Enter the value to be searched: ";
+	cin >> v;
+	int index = linearSearch(array, n, v);
+	if (index == -1)
+	{
+		cout << "NIL" << endl;
+	}
+	else
+	{
+		cout << index << endl;
+	}
+}
